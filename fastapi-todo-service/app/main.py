aec65bc89268b4f00ae44c5e0b4739ba9b95f873
Huy Mạc Quang <57699454+huymq1710@users.noreply.github.com>
2025-07-14 23:12:31 +0700

    Reverse Proxy/ API Gateway — Step 03
    ```json
    "host": "todo-api:8000"
    "connection": "close"
    ```
    
    - “host”: “todo-api:8000” — indicates the hostname and port number of the server to which the request is being sent (todo-api on port 8000). This information is exposed to the client because we forwarded the client requests directly to the todo-api server without any additional configurations.
    “Revealing internal infrastructure details to the client is not ideal.”
    
    In the next step of this project, we will change this behavior.
    - “connection”: “close” — indicates that the connection will be closed after the current transaction is completed. This means that the client or server intends to close the TCP connection after sending or receiving the current HTTP message.
    In an Nginx reverse proxy, this practice can help manage server resources by limiting the number of open connections. It is a part of the connection management strategy to control the lifecycle of connections between clients and servers, helping in the optimization of resource usage and performance improvement.

---
+++ fastapi-todo-service/app/main.py
@@ -0,0 +1,48 @@
+import os
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+from routes.todos import router as todo_router
+
+app = FastAPI(title="Todo API Service")
+
+# add app routers
+app.include_router(todo_router, tags=["todos"])
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def startup():
+    print("Todo Service is starting...")
+    app.mongodb_client = MongoClient(str(os.environ.get("ATLAS_URI")))
+    app.database = app.mongodb_client[str(os.environ.get("DB_NAME"))]
+    try:
+        app.database.command('ping')
+        print("Connected to the MongoDB Database!")
+        print("Todo Service is running...")
+    except ConnectionFailure:
+        print("MongoDB server not available.")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Todo Service is shutting down...")
+
+
+def start():
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("POST", "8000")), reload=True)
---
+++ next-todo-app/actions/todo.ts
@@ -0,0 +1,132 @@
+"use client"
+import * as z from "zod"
+import {CreateTodo, Todo} from "@/schemas/Todo";
+
+// const API_URL = "http://localhost:8000/"
+const API_URL = "http://localhost/" // Use when having nginx reverse proxy
+export const createTodo = async (todo: z.infer<typeof CreateTodo>) => {
+    const validatedFields = CreateTodo.safeParse(todo)
+    if (!validatedFields.success) {
+        const validate_error = JSON.parse(validatedFields.error.message)
+        return {error: validate_error[0].message}
+    }
+
+    const {title} = validatedFields.data
+    const data = {
+        title: title,
+        description: ""
+    }
+
+    try {
+        // create a todo
+        const response = await fetch(API_URL, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        if (response.status == 201) {
+            const result = await response.json()
+            console.log(result)
+            return {success: "Success", todo: result}
+        } else {
+            return {error: "Todo failed!"}
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+export const getTodos = async () => {
+    try {
+        // get todos
+        const response = await fetch(API_URL)
+        if (response.status == 200) {
+            const result = await response.json()
+            return result;
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+export const deleteTodo = async (id: string) => {
+    try {
+        // delete todo
+        const response = await fetch(`${API_URL}${id}`, {
+            method: 'delete'
+        })
+        if (response.status == 204) {
+            return true;
+        }
+    } catch (e) {
+        console.error(e)
+        return false;
+    }
+}
+
+export const completeTodo = async (id: string) => {
+    try {
+        // complete todos
+        const response = await fetch(`${API_URL}${id}/complete`, {
+            method: 'post'
+        })
+        if (response.status == 200) {
+            return true;
+        }
+    } catch (e) {
+        console.error(e)
+        return false;
+    }
+}
+
+export const incompleteTodo = async (id: string) => {
+    try {
+        // withdraw todos
+        const response = await fetch(`${API_URL}${id}/incomplete`, {
+            method: 'post'
+        })
+        if (response.status == 200) {
+            return true;
+        }
+    } catch (e) {
+        console.error(e)
+        return false;
+    }
+}
+
+
+export const updateTodo = async (todo: z.infer<typeof Todo>, id: string) => {
+    const validatedFields = Todo.safeParse(todo)
+    if (!validatedFields.success) {
+        const validate_error = JSON.parse(validatedFields.error.message)
+        return {error: validate_error[0].message}
+    }
+
+    const {title, description} = validatedFields.data
+    const data = {
+        title: title,
+        description: description,
+    }
+
+    try {
+        // update todos
+        const response = await fetch(`${API_URL}${id}`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        if (response.status == 200) {
+            const result = await response.json()
+            console.log(result)
+            return {success: "Success", todo: result}
+        } else {
+            return {error: "Todo failed!"}
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
